c5fbfc917dbe15df02a5b101a5f5416c8bb07341
Wilmerace <wilmerc@imaginacolombia.com>
2023-12-26 18:07:01 -0500

    SE AGREGA USERS

--- _crud/sources.vue
+++ _crud/sources.vue
@@ -81,7 +81,7 @@ export default {
                 requestParams: {filter: {status: 1}}
               }
           },
-          userId: {
+          users: {
             value: null,
             type: 'crud',
             props: {
